e3fd2f1357629d085b0299b02fa061884861fcaf
hyroh5 <hyrlove5598@khu.ac.kr>
2025-05-18 00:33:21 +0900

    5th commit
    stage0 대화 추가

--- Assets/Scripts/TypingEffect_Stage0.cs
+++ Assets/Scripts/TypingEffect_Stage0.cs
@@ -6,21 +6,51 @@ using TMPro;
 public class TypingEffect : MonoBehaviour
 {
     public TextMeshProUGUI dialogueText;  // TMP Text 연결
-    public float typingSpeed = 1f;     // 한 글자당 딜레이
-    public string fullText = "Wake up.... again"; // 출력할 문장
+    public float typingSpeed = 0.5f;     // 한 글자당 딜레이
+    public float deletingSpeed = 0.03f;
+    public float delayBeforeDelete = 1.0f;
+    private string[] messages = {
+        "Wake up.... again",
+        "Let's go to work"
+    }; // 출력할 문장
 
     void Start() // 게임 오브젝트가 처음 활성화될 때 자동 호출
     {
-        StartCoroutine(TypeText()); // 씬 열리자마자 TypeText()가 실행됨
+        StartCoroutine(TypeSequence()); // 씬 열리자마자 TypeSequence()가 실행됨
     }
 
-    IEnumerator TypeText()
+    IEnumerator TypeSequence() // 전체 메세지 순차 출력하는 코루틴 함수
     {
-        dialogueText.text = ""; // TMP 텍스트 초기화 
-        foreach (char c in fullText) // 출력할 전체 문장을 한 글자씩 반복해서 출력 
+        for (int i = 0; i < messages.Length; i++) // 모든 문장 순회
         {
-            dialogueText.text += c; // 기존에 출력된 텍스트 위해 한 글자씩 추가 
-            yield return new WaitForSeconds(typingSpeed); // 타닥타닥의 핵심
+            // 타이핑 효과 
+            yield return StartCoroutine(TypeText(messages[i])); 
+
+            // 잠시 멈춤
+            yield return new WaitForSeconds(delayBeforeDelete);
+
+            // 지우기 (마지막 문장이 아니면 삭제)
+            if (i != messages.Length - 1)
+                yield return StartCoroutine(DeleteText());
+        }
+    }
+
+    IEnumerator TypeText(string sentence) // 문자열을 한 글자씩 출력하는 코루틴 함수
+    {
+        dialogueText.text = ""; // 이전에 저장되었던 텍스트 초기화 
+        foreach (char c in sentence) // 한 글자씩 출력
+        {
+            dialogueText.text += c;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    IEnumerator DeleteText() // 텍스트를 한 글자씩 지우는 코루틴 함수
+    {
+        while (dialogueText.text.Length > 0) // 텍스트가 남아 있을 동안 반복
+        {
+            dialogueText.text = dialogueText.text.Substring(0, dialogueText.text.Length - 1);
+            yield return new WaitForSeconds(deletingSpeed);
         }
     }
 }
